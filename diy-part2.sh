cf60953e5cfb0626d2ce1ce77cb51732397e8410
suen430 <70199984+suen430@users.noreply.github.com>
2022-03-01 12:18:28 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -14,4 +14,4 @@
 sed -i 's/192.168.1.1/192.168.8.2/g' package/base-files/files/bin/config_generate
 
 #2. Clear the login password
-sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.//g' openwrt/package/lean/default-settings/files/zzz-default-settings
+#sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.//g' openwrt/package/lean/default-settings/files/zzz-default-settings
